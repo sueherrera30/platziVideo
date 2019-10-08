dbf2af59d8d6c9f3a235571f7869a9ded26ccbda
sue <sue@MacBook-Pro-de-sue.local>
2019-10-08 01:56:18 -0500

    creating route login test

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../Containers/Home';
+import LogIn from '../Containers/LogIn';
 
 const App = () => (
   <BrowserRouter>
     <Route exact path='/' component={Home} />
+    <Route exact parh='/login' componet={LogIn} />
   </BrowserRouter>
 );
 export default App;
