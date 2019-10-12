53a7864a52a511cb5b9905957186e1d00e3ff854
sue <sue@MacBook-Pro-de-sue.local>
2019-10-11 19:35:06 -0500

    save info in form login

--- src/Containers/LogIn.jsx
+++ src/Containers/LogIn.jsx
@@ -1,44 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+//importamos use state para poder usar hooks
 import { Link } from 'react-router-dom';
 import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
-const LogIn = () => (
-  <section className='login'>
-    <section className='login__container'>
-      <h2>Inicia sesión</h2>
-      <form className='login__container--form'>
-        <input className='input' type='text' placeholder='Correo' />
-        <input className='input' type='password' placeholder='Contraseña' />
-        <button className='button' type='button'>Iniciar sesión</button>
-        <div className='login__container--remember-me'>
-          <label>
-            <input type='checkbox' id='cbox1' value='first_checkbox' />
-            Recuérdame
-          </label>
-          <a href='/'>Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className='login__container--social-media'>
-        <div>
-          <img src={googleIcon} alt='google' />
-          Inicia sesión con Google
-        </div>
-        <div>
-          <img src={twitterIcon} alt='twetter' />
-            Inicia sesión con Twitter
-        </div>
-      </section>
-      <p className='login__container--register'>
-        No tienes ninguna cuenta
-        <Link to='/register'>
-        Regístrate
-        </Link>
-       
-      </p>
-    </section>
-  </section>
-);
+const LogIn = () => {
+  const [form, setValues ] = useState({
+    email: '',
+  });
 
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(form);
+  };
+  return (
+    <section className='login'>
+      <section className='login__container'>
+        <h2>Inicia sesión</h2>
+        <form className='login__container--form' onSubmit={handleSubmit}>
+          <input
+            name='email'
+            className='input'
+            type='text'
+            placeholder='Correo'
+            onChange={handleInput}
+          />
+          <input
+            name='password'
+            className='input'
+            type='password'
+            placeholder='Contraseña'
+            onChange={handleInput}
+          />
+          <button
+            className='button'
+            type='submit'
+          >
+            Iniciar sesión
+          </button>
+          <div className='login__container--remember-me'>
+            <label>
+              <input
+                type='checkbox'
+                id='cbox1'
+                value='first_checkbox'
+              />
+              Recuérdame
+            </label>
+            <a href='/'>Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className='login__container--social-media'>
+          <div>
+            <img src={googleIcon} alt='google' />
+            Inicia sesión con Google
+          </div>
+          <div>
+            <img src={twitterIcon} alt='twetter' />
+              Inicia sesión con Twitter
+          </div>
+        </section>
+        <p className='login__container--register'>
+          No tienes ninguna cuenta
+          <Link to='/register'>
+             Regístrate
+          </Link>
+        </p>
+      </section>
+    </section>
+  );
+};
 export default LogIn;
 
